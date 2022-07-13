845b94a019498059f542f5d6d811979ad9333c3f
INEXX1096 <109241082+INEXX1096@users.noreply.github.com>
2022-07-13 22:00:59 +0100

    Update Exe21.cpp

--- Sort Question/Exe21.cpp
+++ Sort Question/Exe21.cpp
@@ -36,21 +36,19 @@ public:
 };
 
 //recursion method
-class Solution
-{
+class Solution {
 public:
-   ListNode * mergeTwoList(ListNode * list1, ListNode * list2){
-     
-     if(list1 == nullptr){
-       return list2;
-     }else if(list2 == nullptr){
-        return list1;
-     }else if(list1->val <= list2->val){
-       list1->next = mergeTwoList(list1->next,list2)
-         return list1;   
-     }else if (list1->val > list2->val){
-        list2->next = mergeTwolist(list1,list2->next)
-          return list2;
-     }
-   }
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if(list1 == nullptr){
+            return list2;
+        }else if (list2 == nullptr){
+            return list1;
+        }else if(list2->val > list1->val){
+            list1->next = mergeTwoLists(list1->next,list2);
+            return list1;
+        }else{
+            list2->next = mergeTwoLists(list1,list2->next);
+            return list2;
+        }
+    }
 };
