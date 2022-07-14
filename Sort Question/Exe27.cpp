c2d30185bf20aabb05a8d381baf7f49343590576
INEXX1096 <109241082+INEXX1096@users.noreply.github.com>
2022-07-15 00:01:41 +0100

    Create Exe27.cpp

---
+++ Sort Question/Exe27.cpp
@@ -0,0 +1,24 @@
+// website link
+//https://leetcode.com/problems/remove-element/
+
+#include <iostream>
+
+using namespace std;
+ 
+class Solution
+{
+   int removeElement(vector<int>& nums, int val) {
+	   //创建快慢指针(快指针在for loop 中创建)
+	   int slow = 0;
+	   
+	   for(int fast = 0; fast < nums.size(); ++fast) {
+		   if(nums[fast] != val) {
+			   ++slow;
+			   nums[slow] = nums[fast];
+			   
+		   }
+	   }
+	   return slow+1;
+   }
+
+};
