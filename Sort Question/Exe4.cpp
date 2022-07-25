259fead50b8fee890ad17cbc738a2ba8a208098f
INEXX1096 <109241082+INEXX1096@users.noreply.github.com>
2022-07-26 00:32:24 +0100

    Create Exe4.cpp

---
+++ Sort Question/Exe4.cpp
@@ -0,0 +1,22 @@
+//4. Median of Two Sorted Arrays
+
+
+// 暴力解法：合并两个数组并且用遍历的方法
+#include <iostream>
+#cinclude <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+	double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2){
+		//创建两个指针，用来遍历nums1 和nums2 
+		int i = 0, j = 0;
+		
+		for(int i = 0; i < nums1.size(); ++i)
+		{
+			if(nums2[j] >= )
+		}
+	}
+};
