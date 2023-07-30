418966c876ca64ae081bdeeba06bd7a15e9fc553
INEXX <109241082+INEXX1096@users.noreply.github.com>
2023-07-30 22:18:44 +0100

    Update Exe2.cpp

--- Linked List/Exe2.cpp
+++ Linked List/Exe2.cpp
@@ -8,3 +8,34 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+class Solution {
+public:
+  ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+    // Create a new linked list to store the result 
+    ListNode* Result = new ListNode(0);
+    ListNOde* current = Result;
+    int carr = 0;
+
+    while(l1 != NULL || l2 != NULL || carr != 0){
+      int x = l1? l1->val : 0;
+      int y = l2? l2->val : 0;
+      int sum = x + y + carr;
+
+      carr = sum/10;
+      current-> next = new ListNode (sum%10);
+      current = current->next;
+
+      l1 = l1? l1->next: nullptr;
+      l2 = l2? l2->next: nullptr;
+      
+      
+    }
+    return Result->next
+    
+  }
+
+
+
+
+}
