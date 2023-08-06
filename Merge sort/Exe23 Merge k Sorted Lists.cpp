50436ba5c659b04585cd03c82b6a6f8cd5735bb6
INEXX <109241082+INEXX1096@users.noreply.github.com>
2023-08-06 13:42:12 +0100

    Create  Exe23 Merge k Sorted Lists.cpp

---
+++ Merge sort/Exe23 Merge k Sorted Lists.cpp
@@ -0,0 +1,26 @@
+//
+
+class Solution{
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists){
+        if(lists.empty()) return nullptr;
+        while(lists.size() > 1){
+            lists.push_back(MergeTwoLists(lists[0], lists[1]));
+            lists.erase(lists.begin());
+            lists.erase(lists.begin());
+        }
+        return lists.front();
+    }
+private:
+    ListNode* MergeTwoLists(ListNode* l1, ListNode* l2){
+        if(l1 == nullptr) return l2;
+        if(l2 == nullptr) return l1;
+        if(l1->val < l2->val){
+            l1->next = MergeTwoLists(l1->next, l2);
+            return l1;
+        }else{
+            l2->next = MergeTwoLists(l1, l2->next);
+            return l2;
+        }
+    }
+};
